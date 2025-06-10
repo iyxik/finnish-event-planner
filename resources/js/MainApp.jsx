b3a33c967719587a289fdf8b189e8a3da6eaaf7f
fizzaishfaq110 <fizzaishfaq313@gmail.com>
2025-06-10 10:53:15 +0300

    Add: header and footer inside router

--- resources/js/MainApp.jsx
+++ resources/js/MainApp.jsx
@@ -5,16 +5,20 @@ import EventListPage from './Pages/EventListPage';
 import FormPage from './Pages/FormPage';
 import SingleEvent  from './Pages/SingleEvent';
 import "./styles/MainApp.css";
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
     return (
         <Router>
+            <Header/>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/events" element={<EventListPage />} />
                 <Route path="/events/new" element={<FormPage />} />
                 <Route path="/events/:id" element={<SingleEvent />} />
             </Routes>
+            <Footer/>
         </Router>
     );
 }
