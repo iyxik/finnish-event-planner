d87394b3ecb5c74d6a55a63be4f2408a7ca68794
fizzaishfaq110 <fizzaishfaq313@gmail.com>
2025-06-12 15:53:19 +0300

    Add: filter by date UI and fix: sort by date

--- resources/js/Components/Events/Filters.jsx
+++ resources/js/Components/Events/Filters.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import "../../styles/Filters.css";
 
-function Filters({ filterCity, setFilterCity, sortOrder, setSortOrder }) {
+function Filters({ filterCity, setFilterCity, sortOrder, setSortOrder, filterDate,setFilterDate }) {
     return (
         <div className="filter-sort">
+            <div>
             <label>
                 Filter by City:
                 <input type="text" value={filterCity} onChange={(e) => setFilterCity(e.target.value)} placeholder="Enter city" />
             </label>
+            <label>
+                Filter by Date:
+                <input
+                    type="date"
+                    value={filterDate}
+                    onChange={(e) => setFilterDate(e.target.value)}
+                />
+            </label>
+            </div>
             <label>
                 Sort by Date:
                 <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}>
